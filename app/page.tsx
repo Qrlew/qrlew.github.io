55e5503b5222d5c5cd619a2d9cc543108f7919f8
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2023-06-14 23:53:24 +0200

    CHange a color

--- app/page.tsx
+++ app/page.tsx
@@ -253,22 +253,22 @@ let relation = relation.dp_compilation(
           Many connectors to other tools in Differential Privacy should be developped in the coming months.</p>
         </div>
       </div>
-      <div className="w-full max-w-7xl flex flex-col items-left bg-dark text-lighter-green py-10 z-10">
-        <div className="font-display text-sm py-1 text-[#999999]">Licensed under the Apache License, Version 2.0 (the &quot;License&quot;);
+      <div className="w-full flex flex-col items-center bg-dark text-lighter-green py-10 z-10">
+        <div className="w-full max-w-7xl text-sm py-1 text-[#999999]">Licensed under the Apache License, Version 2.0 (the &quot;License&quot;);
           you may not use this file except in compliance with the License.
           You may obtain a copy of the License at
         </div>
-        <div className="font-display text-sm py-1 text-[#999999]">
+        <div className="w-full max-w-7xl text-sm py-1 text-[#999999]">
           <a href="http://www.apache.org/licenses/LICENSE-2.0">http://www.apache.org/licenses/LICENSE-2.0</a>
         </div>
-        <div className="font-display text-sm py-1 text-[#999999]">Unless required by applicable law or agreed to in writing, software
+        <div className="w-full max-w-7xl text-sm py-1 text-[#999999]">Unless required by applicable law or agreed to in writing, software
           distributed under the License is distributed on an &quot;AS IS&quot; BASIS,
           WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
           See the License for the specific language governing permissions and
           limitations under the License.
         </div>
       </div>
-      <div className="flex justify-center">
+      <div className="w-full flex justify-center bg-dark">
           <div className="font-display text-lg py-5">
             Copyright 2023
           </div>
