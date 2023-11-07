c83f6e932192c9054f841ff5ec53cc4e5979a94b
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2023-11-07 02:04:24 +0100

    Updated website

--- app/dp/page.tsx
+++ app/dp/page.tsx
@@ -10,109 +10,63 @@ export default async function Page() {
   return (
     <React.Fragment>
     <Section>
-      <H1>Qrlew <span className="text-light-green">Viewer</span></H1>
-      <SubSection>
+      <H1>Qrlew <span className="text-light-green">Playground</span></H1>
+      <div className="w-full p-3">
         <H2>Simple Dataset and Query</H2>
         <Playground dataset={
 `{
   "tables":[
-    {
-      "name": "data_table",
-      "path": ["schema", "data_table"],
-      "schema": { "fields": [
-        {"name": "a", "data_type": "Float"},
-        {"name": "b", "data_type": "Integer"}
-      ]},
-      "size": 1000
-    }
+     {
+        "name":"user_table",
+        "path":[
+           "schema",
+           "user_table"
+        ],
+        "schema":{
+           "fields":[
+              { "name":"id",  "data_type":"Integer" },
+              { "name":"name", "data_type":"Text" },
+              { "name":"age", "data_type":"Integer" },
+              { "name":"weight", "data_type":"Float" }
+           ]
+        },
+        "size":10000
+     },
+     {
+        "name":"action_table",
+        "path":[
+           "schema",
+           "action_table"
+        ],
+        "schema":{
+           "fields":[
+              { "name":"action", "data_type":"Text" },
+              { "name":"user_id", "data_type":"Integer" },
+              { "name":"duration", "data_type":"Float" }
+           ]
+        },
+        "size":10000
+     }
   ]
 }`
         } query={
-`SELECT * FROM data_table`
-        } dark_mode={true}></Playground>
+`SELECT sum(duration) FROM action_table WHERE duration > 0 AND duration < 24`
+        } synthetic_data={
+`[
+  [ "user_table", "synthetic_user_table" ],
+  [ "action_table", "synthetic_action_table" ]
+]`
+        } protected_entity={
+`[
+  ["user_table",[],"id"],
+  ["action_table",[["user_id","user_table","id"]],"id"]
+]`
+        } epsilon={1} delta={1e-5} dark_mode={true}></Playground>
         <P>Qrlew is an <Link href="https://github.com/Qrlew">open source library</Link> that
           aims to parse and compile SQL queries into an <Link href="https://en.wikipedia.org/wiki/Intermediate_representation">Intermediate Representation (IR)</Link> that
           is well-suited for various rewriting tasks.
           Although it was originally designed for privacy-focused applications, it can be utilized for a wide range of purposes.</P>
-      </SubSection>
-    </Section>
-    <Section color="main-green">
-      <SubSection>
-        <H2>Query with Join</H2>
-        <Playground dataset={
-`{
-  "tables":[
-    {
-      "name": "data_table",
-      "path": ["schema", "data_table"],
-      "schema": { "fields": [
-        {"name": "a", "data_type": "Float"},
-        {"name": "b", "data_type": "Integer"}
-      ]},
-      "size": 1000
-    }
-  ]
-}`
-        } query={
-`SELECT * FROM data_table`
-        } dark_mode={true}></Playground>
-        <P>Qrlew is an <Link href="https://github.com/Qrlew">open source library</Link> that
-          aims to parse and compile SQL queries into an <Link href="https://en.wikipedia.org/wiki/Intermediate_representation">Intermediate Representation (IR)</Link> that
-          is well-suited for various rewriting tasks.
-          Although it was originally designed for privacy-focused applications, it can be utilized for a wide range of purposes.</P>
-      </SubSection>
-    </Section>
-    <Section color="light-green">
-      <SubSection>
-        <H2>Query with Join</H2>
-        <Playground dataset={
-`{
-  "tables":[
-    {
-      "name": "data_table",
-      "path": ["schema", "data_table"],
-      "schema": { "fields": [
-        {"name": "a", "data_type": "Float"},
-        {"name": "b", "data_type": "Integer"}
-      ]},
-      "size": 1000
-    }
-  ]
-}`
-        } query={
-`SELECT * FROM data_table`
-        } dark_mode={true}></Playground>
-        <P>Qrlew is an <Link href="https://github.com/Qrlew">open source library</Link> that
-          aims to parse and compile SQL queries into an <Link href="https://en.wikipedia.org/wiki/Intermediate_representation">Intermediate Representation (IR)</Link> that
-          is well-suited for various rewriting tasks.
-          Although it was originally designed for privacy-focused applications, it can be utilized for a wide range of purposes.</P>
-      </SubSection>
-    </Section>
-    <Section color="lighter-green">
-      <SubSection>
-        <H2>Query with Join</H2>
-        <Playground dataset={
-`{
-  "tables":[
-    {
-      "name": "data_table",
-      "path": ["schema", "data_table"],
-      "schema": { "fields": [
-        {"name": "a", "data_type": "Float"},
-        {"name": "b", "data_type": "Integer"}
-      ]},
-      "size": 1000
-    }
-  ]
-}`
-        } query={
-`SELECT * FROM data_table`
-        } dark_mode={false}></Playground>
-        <P>Qrlew is an <Link href="https://github.com/Qrlew">open source library</Link> that
-          aims to parse and compile SQL queries into an <Link href="https://en.wikipedia.org/wiki/Intermediate_representation">Intermediate Representation (IR)</Link> that
-          is well-suited for various rewriting tasks.
-          Although it was originally designed for privacy-focused applications, it can be utilized for a wide range of purposes.</P>
-      </SubSection>
+      </div>
     </Section>
   </React.Fragment>
   )
--- app/dp/playground.tsx
+++ app/dp/playground.tsx
@@ -6,14 +6,23 @@ import Editor from 'react-simple-code-editor';
 import hljs from 'highlight.js';
 import { Dot } from '../dot';
 
-export default function Playground({dataset: initial_dataset, query: initial_query, dark_mode}: {dataset: string, query: string, dark_mode: boolean}) {
+export default function Playground({dataset: initial_dataset, query: initial_query, synthetic_data: initial_synthetic_data, protected_entity: initial_protected_entity, dark_mode}:
+    {dataset: string, query: string, synthetic_data: string, protected_entity: string, epsilon: number, delta: number, dark_mode: boolean}) {
   const [dataset, setDataset] = useState<string>(initial_dataset);
   const [query, setQuery] = useState<string>(initial_query);
+  const [syntheticData, setSyntheticData] = useState<string>(initial_synthetic_data);
+  const [protectedEntity, setProtectedEntity] = useState<string>(initial_protected_entity);
+  const [epsilon, setEpsilon] = useState<number>(1.0);
+  const [delta, setDelta] = useState<number>(1e-5);
   const [dot, setDot] = useState<string>('');
+  const [dpQuery, setDpQuery] = useState<string>('');
+  const [dpDot, setDpDot] = useState<string>('');
 
   // Init the graph for the first time
   useEffect(() => {updateDot(initial_dataset, initial_query)}, []);
+  useEffect(() => {updateDP(initial_dataset, initial_query, initial_synthetic_data, initial_protected_entity, epsilon, delta)}, []);
   
+  // update input dot
   async function updateDot(dataset: string, query: string) {
     try {
       // const response = await fetch('api/dot', {
@@ -32,14 +41,64 @@ export default function Playground({dataset: initial_dataset, query: initial_que
     }
   };
 
+  // update input dot
+  async function updateDP(dataset: string, query: string, synthetic_data: string, protected_entity: string, epsilon: number, delta: number) {
+    try {
+      // const response = await fetch('api/dot', {
+      const response = await fetch('https://qrlew-zsyaspsckq-od.a.run.app/rewrite_with_differential_privacy_with_dot', {
+        method: 'POST',
+        body: JSON.stringify({
+          dataset: JSON.parse(dataset),
+          query: query,
+          synthetic_data: JSON.parse(synthetic_data),
+          protected_entity: JSON.parse(protected_entity),
+          epsilon: epsilon,
+          delta: delta,
+          dark_mode: dark_mode }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (response.ok) {
+        const dp_query_dot = JSON.parse((await response.json()).value);
+        setDpQuery(dp_query_dot.query);
+        setDpDot(dp_query_dot.dot);
+      } else {
+        console.log("Invalid request");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   async function updateDataset(dataset: string) {
     setDataset(dataset);
     await updateDot(dataset, query);
+    await updateDP(dataset, query, syntheticData, protectedEntity, epsilon, delta);
   }
 
   async function updateQuery(query: string) {
     setQuery(query);
     await updateDot(dataset, query);
+    await updateDP(dataset, query, syntheticData, protectedEntity, epsilon, delta);
+  }
+
+  async function updateSyntheticData(synthetic_data: string) {
+    setSyntheticData(synthetic_data);
+    await updateDP(dataset, query, synthetic_data, protectedEntity, epsilon, delta);
+  }
+
+  async function updateProtectedEntity(protected_entity: string) {
+    setProtectedEntity(protected_entity);
+    await updateDP(dataset, query, syntheticData, protected_entity, epsilon, delta);
+  }
+
+  async function updateEpsilon(epsilon: string) {
+    setEpsilon(JSON.parse(epsilon));
+    await updateDP(dataset, query, syntheticData, protectedEntity, JSON.parse(epsilon), delta);
+  }
+
+  async function updateDelta(delta: string) {
+    setDelta(JSON.parse(delta));
+    await updateDP(dataset, query, syntheticData, protectedEntity, epsilon, JSON.parse(delta));
   }
 
   function highlight(language: string): (code: string) => string {
@@ -47,26 +106,71 @@ export default function Playground({dataset: initial_dataset, query: initial_que
   }
   
   return (
-    <div className="w-full max-w-7xl flex flex-row items-center">
-      <div className="w-6/12 p-3">
-        <p className="text-xl my-3">Dataset definition</p>
-        <Editor value={dataset} onValueChange={updateDataset} highlight={highlight('json')}
-          className="hljs rounded-2xl my-3"
-          padding="1em"
-          style={{ color: "#c9d1d9", background: "#0d1117", fontFamily: "var(--font-fira-code), monospace" }}
-        />
-        <p className="text-xl my-3">Query</p>
-        <Editor value={query} onValueChange={updateQuery} highlight={highlight('sql')}
-          className="hljs rounded-2xl my-3"
-          padding="1em"
-          style={{ color: "#c9d1d9", background: "#0d1117", fontFamily: "var(--font-fira-code), monospace" }}
-        />
-      </div>
-      <div className="w-1/12 p-3 flex flex-col items-center">
-        <FontAwesomeIcon icon={faArrowRight} size="xl" />
-      </div>
-      <div className="w-5/12 p-3">
-        {dot && <Dot source={dot} />}
+    <div className="w-full flex overflow-x-auto">
+      <div className="flex flex-row items-start">
+        <div className="w-[1000px] p-3">
+          <p className="text-xl my-3">Dataset definition</p>
+          <Editor value={dataset} onValueChange={updateDataset} highlight={highlight('json')}
+            className="hljs rounded-2xl my-3"
+            padding="1em"
+            style={{ color: "#c9d1d9", background: "#0d1117", fontFamily: "var(--font-fira-code), monospace" }}
+          />
+          <p className="text-xl my-3">Query</p>
+          <Editor value={query} onValueChange={updateQuery} highlight={highlight('sql')}
+            className="hljs rounded-2xl my-3"
+            padding="1em"
+            style={{ color: "#c9d1d9", background: "#0d1117", fontFamily: "var(--font-fira-code), monospace" }}
+          />
+          <p className="text-xl my-3">Synthetic Data</p>
+          <Editor value={syntheticData} onValueChange={updateSyntheticData} highlight={highlight('json')}
+            className="hljs rounded-2xl my-3"
+            padding="1em"
+            style={{ color: "#c9d1d9", background: "#0d1117", fontFamily: "var(--font-fira-code), monospace" }}
+          />
+          <p className="text-xl my-3">Protected Entity</p>
+          <Editor value={protectedEntity} onValueChange={updateProtectedEntity} highlight={highlight('json')}
+            className="hljs rounded-2xl my-3"
+            padding="1em"
+            style={{ color: "#c9d1d9", background: "#0d1117", fontFamily: "var(--font-fira-code), monospace" }}
+          />
+          <p className="text-xl my-3">Epsilon</p>
+          <Editor value={JSON.stringify(epsilon)} onValueChange={updateEpsilon} highlight={highlight('json')}
+            className="hljs rounded-2xl my-3"
+            padding="1em"
+            style={{ color: "#c9d1d9", background: "#0d1117", fontFamily: "var(--font-fira-code), monospace" }}
+          />
+          <p className="text-xl my-3">Delta</p>
+          <Editor value={JSON.stringify(delta)} onValueChange={updateDelta} highlight={highlight('json')}
+            className="hljs rounded-2xl my-3"
+            padding="1em"
+            style={{ color: "#c9d1d9", background: "#0d1117", fontFamily: "var(--font-fira-code), monospace" }}
+          />
+        </div>
+        <div className="w-[100px] p-3 flex flex-col items-center">
+          <FontAwesomeIcon icon={faArrowRight} size="xl" />
+        </div>
+        <div className="w-[500px] p-3 flex flex-col items-center">
+          <p className="text-xl my-3">Intermediate Representation</p>
+          {dot && <Dot source={dot} />}
+        </div>
+        <div className="w-[100px] p-3 flex flex-col items-center">
+          <FontAwesomeIcon icon={faArrowRight} size="xl" />
+        </div>
+        <div className="w-[1000px] p-3 flex flex-col items-center">
+          <p className="text-xl my-3">Rewriten Intermediate Representation</p>
+          {dpDot && <Dot source={dpDot} />}
+        </div>
+        <div className="w-[100px] p-3 flex flex-col items-center">
+          <FontAwesomeIcon icon={faArrowRight} size="xl" />
+        </div>
+        <div className="w-[1000px] p-3">
+          <p className="text-xl my-3">Rewriten Query</p>
+          <Editor value={dpQuery} onValueChange={()=>{}} highlight={highlight('sql')}
+            className="hljs rounded-2xl my-3"
+            padding="1em"
+            style={{ color: "#c9d1d9", background: "#0d1117", fontFamily: "var(--font-fira-code), monospace" }}
+          />
+        </div>
       </div>
     </div>
   );
